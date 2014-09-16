cc1878f3c455ce94a1f1a605e57a46841890a116
Jess Haddow <jess.haddow@gmail.com>
2014-09-16 11:53:40 -0600

    Completes callbackpractice

--- callbackPractice.js
+++ callbackPractice.js
@@ -4,28 +4,54 @@
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
+var first = function(arr, callback) {
+  callback(arr[0]);
+}
 first(names, function(firstName){
-  console.log('The first name in names is ', firstName)
+  console.log('The first name in names is ', firstName);
 });
 
+var last = function(arr, callback) {
+  callback(arr[arr.length-1]);
+}
 last(names, function(lastName){
   console.log('The last name in names is ', lastName);
 });
 
-contains('Colt', function(yes){
+var contains = function(arr, item, callback) {
+  callback(arr.indexOf(item) > -1);
+}
+
+contains(names, 'Colt', function(yes){
   if(yes){
-    'Colt is in the array';
+    console.log('Colt is in the array');
   } else {
-    'Colt is not in the list';
+    console.log('Colt is not in the list');
   }
 });
+
+var uniq = function (arr, callback){
+  var uniqArr = [];
+  for(var i=0; i < arr.length; i++) {
+    if(arr.indexOf(arr[i], i+1) === -1) {
+      uniqArr.push(arr[i]);
+    }
+  }
+  callback(uniqArr);
+}
 
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
 
+var each = function(arr, callback) {
+  for (var i=0; i < arr.length; i++) {
+    callback(arr[i], i);
+  }
+}
+
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + 'position is ' + item)
+  console.log('The item in the ' + indice + ' position is ' + item)
 });
 
 var users = [
@@ -49,8 +75,17 @@ var users = [
   },
 ];
 
-getUserById('16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address); 
+var getUserById = function(list, id, callback) {
+  for(var i=0; i < list.length; i++) {
+    var user = list[i];
+    if (user['id'] === id) {
+      callback(user);
+      break;
+    }
+  }
+}
+getUserById(users, '16t', function(user){
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
 });
 
 
